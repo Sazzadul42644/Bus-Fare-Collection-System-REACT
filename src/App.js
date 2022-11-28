897d2e6c2d83cb0d163569c3fd5ea32260603de3
Kanta Bhattacharjee <kanto.uv@outlook.com>
2022-11-28 09:28:29 +0600

    station component added data get form api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Head from './Pages/Shared/Header/Head';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import CardPunch from './Pages/DummyRequest/CardPunch';
 import ViewBusList from './Pages/ViewBusList/ViewBusList';
+import ViewStation from './Pages/ViewStation/ViewStation';
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
       <Head />
       <Routes>
         <Route exact path='/' element={<Login />} />
+        <Route exact path='/login' element={<Login />} />
         <Route exact path='/bus-list' element={<ViewBusList />} />
         <Route exact path='/Card-Punch' element={<CardPunch />} />
+        <Route exact path='/stations' element={<ViewStation />} />
       </Routes>
     </Router>
 
---
+++ src/Pages/ViewStation/ViewStation.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const ViewStation = () => {
+    const [stations, setStations] = useState([]);
+
+
+    const getData = (e) => {
+        axios.get("http://127.0.0.1:8000/api/stations")
+            .then(res => {
+                console.log(res.data);
+                setStations(res.data);
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
+    return (
+        <div>
+            <button onClick={getData} className="btn btn-primary mt-5 col-md-4">get station name</button>
+            <div class="mt-5">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">station id</th>
+                            <th scope="col">station name</th>
+                            <th scope="col">latitude</th>
+                            <th scope="col">longitude</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {stations.map(station => (
+                            <tr key={station.id}>
+                                <td>{station.station_id}</td>
+                                <td>{station.station_name}</td>
+                                <td>{station.latitude}</td>
+                                <td>{station.longitude}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    );
+};
+
+export default ViewStation;
